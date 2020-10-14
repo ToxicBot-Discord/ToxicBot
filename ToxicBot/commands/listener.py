df2149e13c8520674f519566c4e6ac1697f2823c
Jigyasu Dhingra <jigyasudhingra@gmail.com>
2020-10-14 14:25:04 +0530

    Issues Resolved

--- ToxicBot/commands/listener.py
+++ ToxicBot/commands/listener.py
@@ -21,6 +21,12 @@ class ToxicBotListener(commands.Cog):
         logger.warning('Logged on as {0}!'.format(self.bot.user.name))
 
     @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send('Welcome {0.mention}.'.format(member))
+
+    @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user:
             return

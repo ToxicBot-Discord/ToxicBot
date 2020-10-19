753d79163d82a710206f2aa1f04b4333837f87f5
zebralt <15049142+Zebralt@users.noreply.github.com>
2020-10-19 10:46:54 +0200

    Update embed.py

--- ToxicBot/helper/embed.py
+++ ToxicBot/helper/embed.py
@@ -1,8 +1,16 @@
+from functools import partial
 from discord import Embed
 
 
-def as_embed(text: str) -> Embed:
+def as_embed(text: str, **kw) -> Embed:
     return Embed(
-        title='ToxixBot says',
-        description=text
+        type='rich',
+        title='ToxicBot says',
+        description=text,
+        **kw
     )
+
+
+error   = partial(as_embed, color=0xff0000)
+warning = partial(as_embed, color=0xffff00)
+info    = partial(as_embed, color=0x0000ff)
